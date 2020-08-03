85caf949a8b26700e44b1666748648996ea1840a
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-08-03 16:08:02 +0700

    reformat code and add loops

--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -4,33 +4,37 @@ import static org.junit.Assert.*;
 
 public class CircularBufferTest {
     @Test
-    public void new_buffer_should_be_empty(){
+    public void new_buffer_should_be_empty() {
         CircularBuffer cb = new CircularBuffer(10);
         boolean result = cb.isEmpty();
         assertTrue(result);
     }
+
     @Test
-    public void new_buffer_size_should_be_10(){
+    public void new_buffer_size_should_be_10() {
         CircularBuffer cb = new CircularBuffer(10);
         boolean result = cb.isFull();
-        assertFalse("Buffer ไม่เต็มนะ",result);
+        assertFalse("Buffer ไม่เต็มนะ", result);
     }
+
     @Test
-    public void write_A_to_buffer_should_read_A(){
+    public void write_A_to_buffer_should_read_A() {
         CircularBuffer cb = new CircularBuffer(10);
         cb.writeData("A");
-        assertEquals("A",cb.readData());
+        assertEquals("A", cb.readData());
     }
+
     @Test
-    public void write_A_then_B_to_buffer_should_read_A_then_B(){
+    public void write_A_then_B_to_buffer_should_read_A_then_B() {
         CircularBuffer cb = new CircularBuffer(10);
         cb.writeData("A");
         cb.writeData("B");
-        assertEquals("A",cb.readData());
-        assertEquals("B",cb.readData());
+        assertEquals("A", cb.readData());
+        assertEquals("B", cb.readData());
     }
+
     @Test
-    public void writing_10_times_should_make_buffer_full(){
+    public void writing_10_times_should_make_buffer_full() {
         CircularBuffer cb = new CircularBuffer(10);
         cb.writeData("A");
         cb.writeData("A");
@@ -44,31 +48,20 @@ public class CircularBufferTest {
         cb.writeData("A");
         assertTrue(cb.isFull());
     }
+
     @Test
-    public void writing_11_times_should_overwrite_first_string(){
+    public void writing_11_times_should_overwrite_first_string() {
         CircularBuffer cb = new CircularBuffer(10);
-        cb.writeData("1");
-        cb.writeData("2");
-        cb.writeData("3");
-        cb.writeData("4");
-        cb.writeData("5");
-        cb.writeData("6");
-        cb.writeData("7");
-        cb.writeData("8");
-        cb.writeData("9");
-        cb.writeData("10");
-        assertEquals("1",cb.readData());
-        cb.readData();
-        cb.readData();
-        cb.readData();
-        cb.readData();
-        cb.readData();
-        cb.readData();
-        cb.readData();
-        cb.readData();
+        for (int i = 1; i <= 10; i++) {
+            cb.writeData(Integer.toString(i));
+        }
+        assertEquals("1", cb.readData());
+        for (int i = 1; i <= 8; i++) {
+            cb.readData();
+        }
         cb.writeData("11");
         cb.readData();
-        assertEquals("11",cb.readData());
+        assertEquals("11", cb.readData());
 
     }
 }
