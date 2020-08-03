15a2b297d3dab04db35495e117cf8575fb5612ed
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-08-03 11:46:03 +0700

    change substring 2 to lowercase

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
 
     public String sayHi(String name) {
         String substring1 = name.substring(0,1).toUpperCase();
-        String substring2 = name.substring(1,name.length());
+        String substring2 = name.substring(1,name.length()).toLowerCase();
         String capitalizedName = substring1 + substring2;
         return "Hello " + capitalizedName;
     }
