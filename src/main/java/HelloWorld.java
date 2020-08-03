aad15b1a92188e320899d83413efb3368391cd54
Bannavith Khuhaprema <bannavithkhu@mathregit.group>
2020-08-03 11:37:31 +0700

    initial workshop commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+    }
+
+    public String sayHi() {
+        return "Hello World";
+    }
+
+    public String sayHi(String name) {
+        String substring1 = name.substring(0,1).toUpperCase();
+        String substring2 = name.substring(1,name.length());
+        String capitalizedName = substring1 + substring2;
+        return "Hello " + capitalizedName;
+    }
+}
